083445a58ca6124c1017c9506f959d9212bcdde9
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-09-29 15:14:26 -0300

    req 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const infoSpecie = data.species.find((specie) => specie.name === animal);
+  return infoSpecie.residents.every((residente) => residente.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
