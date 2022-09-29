d7ef4d49de2330ac83123a5c1c3924c9441aa2f4
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-09-29 15:42:52 -0300

    req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return data.employees.some((employee) => employee.managers)
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const employeesManagers = data.employees.filter((employee) =>
+    employee.managers.includes(managerId));
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employeesManagers.map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
