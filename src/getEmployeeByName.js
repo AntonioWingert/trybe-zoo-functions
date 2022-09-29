ab594657b95f6a8a29a5bed56fb71fb91310f6d0
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-09-29 15:31:35 -0300

    run lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,7 +2,8 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   if (employeeName === undefined) return {};
-  return data.employees.find((employee) => (employee.firstName === employeeName || employee.lastName === employeeName))
+  return data.employees.find((employee) => {
+    (employee.firstName === employeeName || employee.lastName === employeeName)});
 }
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,6 +3,6 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   if (ids === undefined) return [];
   return ids.map((element) => data.species.find((specie) => specie.id === element));
-};
+}
 
 module.exports = getSpeciesByIds;
