7649f6535ef893c576e4373a713859d96c1ee271
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-09-29 16:25:18 -0300

    req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const allAnimals = () => {
+  const object = {};
+  data.species.forEach((actual) => { object[actual.name] = actual.residents.length; });
+  return object;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) return allAnimals();
+  const { specie, sex } = animal;
+  const especiesD = data.species.find((actualAnimal) => actualAnimal.name === specie);
+  if (sex !== undefined) {
+    const residentsSex = especiesD.residents.filter((actualResidente) => actualResidente.sex === sex);
+    return residentsSex.length;
+  }
+  return especiesD.residents.length;
 }
 
 module.exports = countAnimals;
