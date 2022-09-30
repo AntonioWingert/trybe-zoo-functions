3794b511fe26f163834a2df71c7472d0ab8a5210
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-09-30 13:51:57 -0300

    finalizando o projeto

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,69 @@
 const data = require('../data/zoo_data');
 
+function getAnimalsOfAZone(zone) {
+  const speciesOfZone = data.species.filter((species) =>
+    species.location === zone);
+  return speciesOfZone;
+}
+
+function getResidentsOfSpecies(species) {
+  const object = {
+    species: species.name,
+    all: [],
+    male: [],
+    female: [],
+  };
+  species.residents.forEach((residents) => {
+    object.all.push(residents.name);
+    if (residents.sex === 'male') object.male.push(residents.name);
+    if (residents.sex === 'female') object.female.push(residents.name);
+  });
+  return object;
+}
+
+function noParam(object) {
+  Object.keys(object).forEach((zone) => {
+    const speciesOfZone = getAnimalsOfAZone(zone);
+    speciesOfZone.forEach((spe) => object[zone].push(spe.name));
+  });
+  return object;
+}
+
+function sortObject(object) {
+  Object.keys(object).forEach((zone) => {
+    object[zone].forEach((animal) => {
+      animal[Object.keys(animal)[0]]
+        .sort((a, b) => a.localeCompare(b));
+    });
+  });
+  return object;
+}
+
+function insertNames(sex, specieData) {
+  const names = {};
+  if (sex === 'male') names[specieData.species] = specieData.male;
+  if (sex === 'female') names[specieData.species] = specieData.female;
+  if (!sex) names[specieData.species] = specieData.all;
+  return names;
+}
+
+function auxFunction(object, sex) {
+  Object.keys(object).forEach((zone) => {
+    const speciesOfZone = getAnimalsOfAZone(zone);
+    speciesOfZone.forEach((specie) => {
+      const specieData = getResidentsOfSpecies(specie);
+      object[zone].push(insertNames(sex, specieData));
+    });
+  });
+}
 function getAnimalMap(options) {
-  // seu código aqui
+  const object = { NE: [], NW: [], SE: [], SW: [] };
+  if (!options) return noParam(object);
+  const { sex, sorted, includeNames } = options;
+  if (!includeNames) return noParam(object);
+  if (includeNames) auxFunction(object, sex);
+  if (sorted) sortObject(object);
+  return object;
 }
 
 module.exports = getAnimalMap;
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function validateId(id) {
+  if (!data.employees.some((e) => e.id === id)) {
+    throw new Error('Informações inválidas');
+  }
+  return true;
 }
 
+function validateName(name) {
+  if (!data.employees.some((e) => e.firstName === name || e.lastName === name)) {
+    throw new Error('Nome inválido');
+  }
+  return true;
+}
+
+function objCreate({ id: objId, firstName: objFN,
+  lastName: objLN, responsibleFor: objRPS }) {
+  const newObject = {
+    id: objId,
+    fullName: `${objFN} ${objLN}`,
+    species: objRPS.map((e) =>
+      data.species.find((e2) => e2.id === e).name),
+    locations: objRPS.map((e) =>
+      data.species.find((e2) => e2.id === e).location),
+  };
+  return newObject;
+}
+
+function getEmployeesCoverage({ name: empName, id: empId } = 0) {
+  if (!empName && !empId) {
+    const newArray = [];
+    data.employees.forEach((element) => newArray.push(objCreate(element)));
+    return newArray;
+  } if (empName && !empId) {
+    validateName(empName);
+    const employeeObject = data.employees.find((e) =>
+      e.firstName === empName || e.lastName === empName);
+    return objCreate(employeeObject);
+  }
+  validateId(empId);
+  const employeeObject = data.employees.find((e) => e.id === empId);
+  return objCreate(employeeObject);
+}
 module.exports = getEmployeesCoverage;
