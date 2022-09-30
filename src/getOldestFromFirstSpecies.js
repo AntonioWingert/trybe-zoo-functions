1faf215ba4966f3ba4188542a2db47c4b84bc761
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-09-30 13:28:36 -0300

    req 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((e) => e.id === id);
+  const firstRespId = employee.responsibleFor[0];
+  const firstRespSpecies = data.species.find((specie) =>
+    specie.id === firstRespId);
+  const animal = firstRespSpecies.residents.reduce((acc, curr) => {
+    if (curr.age > acc.age) return curr;
+    return acc;
+  });
+  const { name, sex, age } = animal;
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
